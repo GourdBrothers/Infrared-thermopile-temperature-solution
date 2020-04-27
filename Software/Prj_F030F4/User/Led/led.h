d31ea79f8572367e64a663714aec62b4fecd1458
xcwk_xl <1410676060@qq.com>
2020-04-27 16:09:29 +0800

    Add circuit board design files:PCB_031K6_V10_AD17.PcbDoc

--- Software/Prj_F030F4/User/Led/led.h
+++ Software/Prj_F030F4/User/Led/led.h
@@ -4,7 +4,7 @@
 #include "stm32f0xx.h"
 
 #define LED_OFF                       GPIOA->BSRR = 0x10
-#define LED_ON                        GPIOA->BRR = 0x10 
+#define LED_ON                        GPIOA->BRR  = 0x10 
 #define LED_TURN                      GPIOA->ODR ^= 0x10
 
 void LED_Init(void);
